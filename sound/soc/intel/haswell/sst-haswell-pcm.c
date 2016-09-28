edf7462eec832366b09281d05a7b20fe8effe92c
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2016-09-28 16:13:25 +0100

    ASoC: Intel-tmp: Change topology to match FW.
    TODO: this will be deleted once topology support is added.
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- sound/soc/intel/haswell/sst-haswell-pcm.c
+++ sound/soc/intel/haswell/sst-haswell-pcm.c
@@ -33,7 +33,7 @@
 #include "../common/sst-dsp-priv.h"
 #include "../common/sst-dsp.h"
 
-#define HSW_PCM_COUNT		1
+#define HSW_PCM_COUNT		2
 #define HSW_VOLUME_MAX		(1 << 16)	/* 0dB */
 
 #define SST_OLD_POSITION(d, r, o) ((d) +		\
@@ -417,6 +417,18 @@ static const struct snd_kcontrol_new hsw_volume_controls[] = {
 	SOC_DOUBLE_EXT_TLV("Master Playback Volume", 0, 0, 8,
 		ARRAY_SIZE(volume_map) - 1, 0,
 		hsw_volume_get, hsw_volume_put, hsw_vol_tlv),
+	/* PCM0 volume */
+	SOC_DOUBLE_EXT_TLV("PCM0 Playback Volume", 0, 0, 8,
+		ARRAY_SIZE(volume_map) - 1, 0,
+		hsw_stream_volume_get, hsw_stream_volume_put, hsw_vol_tlv),
+	/* PCM1 volume */
+	SOC_DOUBLE_EXT_TLV("PCM1 Playback Volume", 1, 0, 8,
+		ARRAY_SIZE(volume_map) - 1, 0,
+		hsw_stream_volume_get, hsw_stream_volume_put, hsw_vol_tlv),
+	/* Mic Capture volume */
+	SOC_DOUBLE_EXT_TLV("Mic Capture Volume", 4, 0, 8,
+		ARRAY_SIZE(volume_map) - 1, 0,
+		hsw_stream_volume_get, hsw_stream_volume_put, hsw_vol_tlv),
 #if 0
 	/* Offload 0 volume */
 	SOC_DOUBLE_EXT_TLV("Media0 Playback Volume", 1, 0, 8,
@@ -984,9 +996,10 @@ static int hsw_pcm_new(struct snd_soc_pcm_runtime *rtd)
 #define HSW_FORMATS \
 	(SNDRV_PCM_FMTBIT_S24_LE | SNDRV_PCM_FMTBIT_S16_LE)
 
+// TODO: this will come from topology
 static struct snd_soc_dai_driver hsw_dais[] = {
 	{
-		.name  = "System Pin",
+		.name  = "PCM0 Pin",
 		.id = HSW_PCM_DAI_ID_SYSTEM,
 		.playback = {
 			.stream_name = "System Playback",
@@ -1003,6 +1016,19 @@ static struct snd_soc_dai_driver hsw_dais[] = {
 			.formats = SNDRV_PCM_FMTBIT_S24_LE | SNDRV_PCM_FMTBIT_S16_LE,
 		},
 	},
+	{
+		/* PCM */
+		.name  = "PCM1 Pin",
+		.id = HSW_PCM_DAI_ID_OFFLOAD0,
+		.playback = {
+			.stream_name = "PCM1 Playback",
+			.channels_min = 2,
+			.channels_max = 2,
+			.rates = SNDRV_PCM_RATE_48000,
+			.formats = SNDRV_PCM_FMTBIT_S24_LE | SNDRV_PCM_FMTBIT_S16_LE,
+		},
+	},
+#if 0
 	{
 		/* PCM */
 		.name  = "Offload0 Pin",
@@ -1038,30 +1064,49 @@ static struct snd_soc_dai_driver hsw_dais[] = {
 			.formats = SNDRV_PCM_FMTBIT_S24_LE | SNDRV_PCM_FMTBIT_S16_LE,
 		},
 	},
+#endif
 };
 
+// TODO: this will come from topology
 static const struct snd_soc_dapm_widget widgets[] = {
 
 	/* Backend DAIs  */
-	SND_SOC_DAPM_AIF_IN("SSP0 CODEC IN", NULL, 0, SND_SOC_NOPM, 0, 0),
-	SND_SOC_DAPM_AIF_OUT("SSP0 CODEC OUT", NULL, 0, SND_SOC_NOPM, 0, 0),
-	SND_SOC_DAPM_AIF_IN("SSP1 BT IN", NULL, 0, SND_SOC_NOPM, 0, 0),
-	SND_SOC_DAPM_AIF_OUT("SSP1 BT OUT", NULL, 0, SND_SOC_NOPM, 0, 0),
+	SND_SOC_DAPM_AIF_IN("SSP CODEC IN", NULL, 0, SND_SOC_NOPM, 0, 0),
+	SND_SOC_DAPM_AIF_OUT("SSP CODEC OUT", NULL, 0, SND_SOC_NOPM, 0, 0),
+//	SND_SOC_DAPM_AIF_IN("SSP1 BT IN", NULL, 0, SND_SOC_NOPM, 0, 0),
+//	SND_SOC_DAPM_AIF_OUT("SSP1 BT OUT", NULL, 0, SND_SOC_NOPM, 0, 0),
 
-	/* Global Playback Mixer */
-	SND_SOC_DAPM_MIXER("Playback VMixer", SND_SOC_NOPM, 0, 0, NULL, 0),
+	/* mixers */
+	SND_SOC_DAPM_MIXER("Playback Mixer", SND_SOC_NOPM, 0, 0, NULL, 0),
+
+	/* Volumes */
+	SND_SOC_DAPM_PGA("Playback Mixer Volume", SND_SOC_NOPM, 0, 0, NULL, 0),
+	SND_SOC_DAPM_PGA("Capture Volume", SND_SOC_NOPM, 0, 0, NULL, 0),
+	SND_SOC_DAPM_PGA("PCM0 Playback Volume", SND_SOC_NOPM, 0, 0, NULL, 0),
+	SND_SOC_DAPM_PGA("PCM1 Playback Volume", SND_SOC_NOPM, 0, 0, NULL, 0),
 };
 
+// TODO: this will come from topology
 static const struct snd_soc_dapm_route graph[] = {
 
+#if 1
 	/* Playback Mixer */
-	{"Playback VMixer", NULL, "System Playback"},
-	{"Playback VMixer", NULL, "Offload0 Playback"},
-	{"Playback VMixer", NULL, "Offload1 Playback"},
+	{"PCM0 Playback Volume", NULL, "System Playback"},
+	{"PCM1 Playback Volume", NULL, "PCM1 Playback"},
+	{"Playback Mixer", NULL, "PCM0 Playback Volume"},
+	{"Playback Mixer", NULL, "PCM1 Playback Volume"},
+	{"Playback Mixer Volume", NULL, "Playback Mixer"},
+	{"SSP CODEC OUT", NULL, "Playback Mixer Volume"},
 
-	{"SSP0 CODEC OUT", NULL, "Playback VMixer"},
+	{"Capture Volume", NULL, "SSP CODEC IN"},
+	{"Analog Capture", NULL, "Capture Volume"},
+#else
+	{"Playback Volume", NULL, "System Playback"},
+	{"SSP2 CODEC OUT", NULL, "Playback Volume"},
 
-	{"Analog Capture", NULL, "SSP0 CODEC IN"},
+	{"Capture Volume", NULL, "SSP2 CODEC IN"},
+	{"Analog Capture", NULL, "Capture Volume"},
+#endif
 };
 
 static int hsw_pcm_probe(struct snd_soc_platform *platform)
@@ -1242,7 +1287,7 @@ static int hsw_pcm_runtime_suspend(struct device *dev)
 return 0;
 	if (pdata->pm_state >= HSW_PM_STATE_RTD3)
 		return 0;
-
+#if 0
 	/* fw modules will be unloaded on RTD3, set flag to track */
 	if (sst_hsw_is_module_active(hsw, SST_HSW_MODULE_WAVES)) {
 		ret = sst_hsw_module_disable(hsw, SST_HSW_MODULE_WAVES, 0);
@@ -1250,6 +1295,7 @@ return 0;
 			return ret;
 		sst_hsw_set_module_enabled_rtd3(hsw, SST_HSW_MODULE_WAVES);
 	}
+#endif
 	hsw_pcm_suspend(dev);
 	pdata->pm_state = HSW_PM_STATE_RTD3;
 
